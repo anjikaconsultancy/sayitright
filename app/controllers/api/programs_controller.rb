dbfaaf657d147fb8f599f97179b554cfdb2c431c
Sumit <sumit.raghuwanshi192@gmail.com>
2020-06-02 14:56:53 +0530

    Fix allocation dropdown issue

--- app/controllers/api/programs_controller.rb
+++ app/controllers/api/programs_controller.rb
@@ -10,33 +10,32 @@ class Api::ProgramsController < Api::AccessController
       # @programs = current_site.programs.order_by([:publish_at,:desc]).page(params[:page]).per(10)
       @programs = current_site.programs.order("publish_at DESC").page(params[:page]).per(10)
       # @programs = pager(@programs,50)
-      
+
       respond_with(@programs)
     else
-      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")  
-    end  
+      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")
+    end
   end
-  
+
   def show
     @program = current_site.programs.find(params[:id])
     if [:administrator,:manager,:moderator].include?(current_user.role) or @program.user.id == current_user.id
       respond_with(@program)
     else
-      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")  
+      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")
     end
   end
-  
-  def new
 
-    if current_user.role != :user  
+  def new
+    if current_user.role != :user
       @program = Program.new
       @program.site = current_site
       @program.user = current_user
       @program.title = "New Program"
-      
+
       #setup some default allocations
-      @program.allocations.build(site: current_site,status: :published)  
-      @program.allocations.build(site: current_site,user: current_user,status: :published)  
+      @program.allocations.build(site: current_site,status: :published)
+      @program.allocations.build(site: current_site,user: current_user,status: :published)
       
       #setup an empty text fragment  
       @program.fragments.build(content: "Enter some text.")
@@ -44,16 +43,11 @@ class Api::ProgramsController < Api::AccessController
       respond_with(@program)
     else
       raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")  
-    end  
-
+    end
   end
-  
-  
 
-
-
-  def create  
-    if current_user.role != :user  
+  def create
+    if current_user.role != :user
       @program = Program.new
       @program.site = current_site
       @program.user = current_user
@@ -63,26 +57,22 @@ class Api::ProgramsController < Api::AccessController
 
       respond_with(@program)
     else
-      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")  
-    end 
+      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")
+    end
   end
 
-  
-  def update  
+  def update
     @program = current_site.programs.find(params[:id])
     if [:administrator,:manager,:moderator].include?(current_user.role) or @program.user.id == current_user.id
+      @program.update_attributes!(resource_params)
 
-      @program.update_attributes(resource_params)  
-      
       respond_with(@program)
     else
-      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")  
-    end 
+      raise ActiveResource::UnauthorizedAccess.new(t "errors.unauthorized")
+    end
   end
-  
-  
-  private
 
+  private
 
   def resource_params
     # Copy attributes to required keys
@@ -100,50 +90,50 @@ class Api::ProgramsController < Api::AccessController
         allocation[:site_id]    = allocation[:site][:id]    if allocation.try(:[],:site).try(:[],:id).present?    
         allocation[:station_id] = allocation[:station][:id] if allocation.try(:[],:station).try(:[],:id).present?   
         allocation[:channel_id] = allocation[:channel][:id] if allocation.try(:[],:channel).try(:[],:id).present?    
-        allocation[:user_id]    = allocation[:user][:id]    if allocation.try(:[],:user).try(:[],:id).present?    
-  
+        allocation[:user_id]    = allocation[:user][:id]    if allocation.try(:[],:user).try(:[],:id).present?
+
         # TODO - Remove this if its not allowed
-        if true == false
-          true
-        else
-          false
-        end  
+        # if true == false
+        #   true
+        # else
+        #   false
+        # end
       end
     end
-          
-    # Fix fragments and segments embedded clips 
+
+    # Fix fragments and segments embedded clips
     if params[:segments_attributes]
       params[:segments_attributes].each do |segment|
-        segment[:clip_id]    = segment[:clip][:id]    if segment.try(:[],:clip).try(:[],:id).present?          
+        segment[:clip_id]    = segment[:clip][:id]    if segment.try(:[],:clip).try(:[],:id).present?
       end
     end
-    if params[:fragments_attributes]
-      params[:fragments_attributes].each do |fragment|      
-        fragment[:clip_id]    = fragment[:clip][:id]    if fragment.try(:[],:clip).try(:[],:id).present?          
-      end
-    end
-  
+    # if params[:fragments_attributes]
+    #   params[:fragments_attributes].each do |fragment|
+    #     fragment[:clip_id]    = fragment[:clip][:id]    if fragment.try(:[],:clip).try(:[],:id).present?
+    #   end
+    # end
+
     # List of what were going to permit
     permits = []
-        
+
     # Administrators, Managers can edit content if its on there own site, and owners can edit content
     if (@program.site.id == current_site.id and [:administrator,:manager].include?(current_user.role)) or @program.user.id == current_user.id
       element_content = [:id,:kind,:position,:content,:url,:clip_id,:_destroy]
       
       permits.concat [:name,:title,:rel,:summary,:language,:status,:tags,:publish_at]
       permits << {segments_attributes: element_content}
-      permits << {fragments_attributes: element_content}
+      # permits << {fragments_attributes: element_content}
     end
-    
+
     # First permit allocations access to moderators on any site or owners
     if [:administrator,:manager,:moderator].include?(current_user.role) or @program.user.id == current_user.id
       # TODO - make sure we can only delete the allocation if allowed
-      permits << {allocations_attributes: [:id,:site_id,:station_id,:channel_id,:user_id,:status,:_destroy]}
+      permits << {allocations_attributes: [:id,:site_id,:channel_id,:status,:_destroy]}
     end
-      
-        
+
     # Splat in the permits parameters
     params.permit(*permits)
-  end  
+
+  end
 
 end
--- app/views/api/allocations/index.rabl
+++ app/views/api/allocations/index.rabl
@@ -1,10 +1,20 @@
-collection @allocations, :xml_root=>"allocations" #=>"allocations",:object_root=>false
+collection @allocations, :xml_root=>"allocations" #, :object_root=>false
+
+attributes :status
 
 child :site=>"site" do
   node(:id) { |o| o.id.to_s }
   attributes :title, :link
 end
+
 child :channel=>"channel" do
-  node(:id) { |o| o.id.to_s }
-  attributes :title, :link
+  if @object[:channel].nil?
+    node(:id) { |o| o.id.to_s }
+
+    attributes :title, :link
+  else
+    node(:title) { 'Default' }
+
+    attributes :title, :link
+  end
 end
